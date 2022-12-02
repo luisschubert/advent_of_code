c21f411c4460662b090f90a2639d23d07889432b
Luis-Johannes Schubert <lschubert@tesla.com>
2022-12-01 16:32:57 -0800

    fixup day 1

--- 2022/day_1/day_1.py
+++ 2022/day_1/day_1.py
@@ -1,20 +1,23 @@
 import os
 
-
-res = []
 res_sum = []
 intermediary = []
 with open("input.txt") as file:
     for l in file:
         if l == "\n":
-            res.append(intermediary)
             res_sum.append(sum(intermediary))
             intermediary = []
         else:
             intermediary.append(int(l.strip("\n")))
 
-for i in res:
-    print(i)
+res_sum_sorted = res_sum
+res_sum_sorted.sort(reverse=True)
 
+top_three = res_sum_sorted[:3]
+print(top_three)
 
-print(max(res_sum))
+print("question 1 result")
+print(res_sum_sorted[0])
+
+print("question 2 result")
+print(sum(top_three))
