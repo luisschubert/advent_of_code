9915636533aacfeddfd2a7f43771d6ed52330bf9
Luis-Johannes Schubert <lschubert@tesla.com>
2022-12-03 13:02:01 -0800

    added day 1 part 1

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let depth_measurements = fs::read_to_string("/Users/lschubert/Documents/advent_of_code/2021/day_1/input_day1.txt")
+        .expect("Should have been able to read the file");
+
+    let mut line_count:u32 = 0;
+    let mut last_measurement:u32 = 0;
+    let mut increment_count: u32 = 0;
+    for line in depth_measurements.lines() {
+        let depth_measurement: u32 = line.parse::<u32>().unwrap();
+        if (line_count != 0) && (depth_measurement >last_measurement){
+            increment_count += 1;
+        }
+        last_measurement = depth_measurement;
+        line_count += 1;
+
+    }
+
+    println!("the increment count is {increment_count}");
 }
