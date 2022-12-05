cca232612fa0a2927c723297740ff6956c8b0072
Luis-Johannes Schubert <lschubert@tesla.com>
2022-12-05 10:24:52 -0800

    first rust solution working

--- 2021/day_1/src/main.rs
+++ 2021/day_1/src/main.rs
@@ -1,15 +1,13 @@
 use std::fs;
 
-fn main() {
-    let depth_measurements = fs::read_to_string("/Users/lschubert/Documents/advent_of_code/2021/day_1/input_day1.txt")
-        .expect("Should have been able to read the file");
-
+fn part_1(depth_measurements: &String) -> u32
+{
     let mut line_count:u32 = 0;
     let mut last_measurement:u32 = 0;
     let mut increment_count: u32 = 0;
     for line in depth_measurements.lines() {
         let depth_measurement: u32 = line.parse::<u32>().unwrap();
-        if (line_count != 0) && (depth_measurement >last_measurement){
+        if (line_count != 0) && (depth_measurement > last_measurement){
             increment_count += 1;
         }
         last_measurement = depth_measurement;
@@ -17,5 +15,37 @@ fn main() {
 
     }
 
-    println!("the increment count is {increment_count}");
+    increment_count
+}
+
+fn part_2(depth_measurements: &String) -> u32
+{
+    let mut line_count:u32 = 0;
+    let mut last_measurement:u32 = 0;
+    let mut increment_count: u32 = 0;
+    for line in depth_measurements.lines() {
+        let depth_measurement: u32 = line.parse::<u32>().unwrap();
+        if (line_count != 0) && (depth_measurement > last_measurement){
+            increment_count += 1;
+        }
+        last_measurement = depth_measurement;
+        line_count += 1;
+
+    }
+
+    increment_count
+}
+
+
+fn main() {
+    let depth_measurements = fs::read_to_string("/Users/lschubert/Documents/advent_of_code/2021/day_1/input_day1.txt")
+        .expect("Should have been able to read the file");
+    
+    let part_1_answer = part_1(&depth_measurements);
+
+    let part_2_answer = part_2(&depth_measurements);
+
+    println!("the increment count for part_1 is {part_1_answer}");
+
+    println!("the increment count for part_2 is {part_2_answer}");
 }
